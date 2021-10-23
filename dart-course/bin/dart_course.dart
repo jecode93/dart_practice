575c120badf769fc122bcbaa6fe387c6eb6bae84
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-10-23 12:50:14 -0400

    Compare to list to verify common items and return true or false

--- dart-course/bin/dart_course.dart
+++ dart-course/bin/dart_course.dart
@@ -1,10 +1,23 @@
 void main() {
-  String name = 'Hello I\'m Jean Emmanuel Cadet';
+  List<String> list1 = ['a', 'b', 'c', 'd', 'e', 'f'];
+  List<String> list2 = ['g', 'h', 'i', 'a']; // Return true
+  List<String> list3 = [
+    'g',
+    'h',
+    'i',
+    'j'
+  ]; // Return false, if we replace list2 by list3 in the parametter.
 
-  print(reverseString(name));
+  print(verifyCommonItemInList(list1, list2));
 }
 
-String reverseString(String valueToReverse) {
-  var val = valueToReverse.runes.toList();
-  return String.fromCharCodes(val.reversed);
+bool verifyCommonItemInList(List<String> l1, List<String> l2) {
+  for (int i = 0; i < l1.length; i++) {
+    for (int j = 0; j < l2.length; j++) {
+      if (l1[i] == l2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
