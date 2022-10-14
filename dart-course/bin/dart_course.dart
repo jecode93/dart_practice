d6dd95a6a4eddb5a3d34a15ee504b5bbf4f63364
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-10-14 04:14:50 -0400

    Add functions to reversed content from a given value.

--- dart-course/bin/dart_course.dart
+++ dart-course/bin/dart_course.dart
@@ -3,8 +3,13 @@ void main() {
 
   List<int> list1 = [2, 5, 6, 2, 7, 8, 2, 3, 7, 5, 10]; //Should return 2
   List<int> list2 = [2, 5, 6, 7, 8, 3, 10]; //Should return nothing
+  String name = "Hello I'm Jean Emmanuel Cadet";
 
   print(firstRecurringCharacter(list1));
+  print("\n");
+  print(reverseContentFromString(name));
+  print("\n");
+  print(reverseContentFromListInt(list2));
 }
 
 int? firstRecurringCharacter(List<int> l1) {
@@ -16,4 +21,14 @@ int? firstRecurringCharacter(List<int> l1) {
     }
   }
   return null;
+}
+
+//Function to Reverse content from a given list
+String reverseContentFromString(String valueToReverse) {
+  var valeur = valueToReverse.runes.toList();
+  return String.fromCharCodes(valeur.reversed);
+}
+
+Iterable<int> reverseContentFromListInt(List<int> valueToReverse) {
+  return valueToReverse.reversed;
 }
